ae5a040e06a299dda50a846415eecf6d73ab5c6e
yuhs0383 <yuhs0383@naver.com>
2022-06-24 10:02:12 +0900

    질병 통계 페이지 수정

--- ClientApp/src/components/ShowData.js
+++ ClientApp/src/components/ShowData.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./ShowData.css";
 import Sidebar from "./Sidebar/Sidebar";
+import SeoulChart from "./SeoulChart";
 
 function ShowData() {
   return (
     <div className="showData">
       <Sidebar width={300} />
       <div className="show-container">
+        <SeoulChart />
         <img
           className="show-img"
           src="https://forward.nhn.com/2020/seoul/hands-on-labs/toastui.chart-dashboard/_images/step08.png"
