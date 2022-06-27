802f471d776bc49ce835103d60abc6919727b572
yuhs0383 <yuhs0383@naver.com>
2022-06-27 09:45:44 +0900

    데이터 연결

--- ClientApp/src/components/SeoulChart.js
+++ ClientApp/src/components/SeoulChart.js
@@ -1,12 +1,12 @@
 import React, { Component, useReducer } from 'react';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
@@ -18,63 +18,63 @@ Object.freeze(ACTION_TYPE);
 
 
 const dataReducer = (state, action) => {
-    switch (action.type) {
-        case ACTION_TYPE.ALL_LIST:
-        return {
-          ...state,
-          dataList: action.dataList,
-          loading: action.loading,
-        };
-      case ACTION_TYPE.CHART_DATA_LOADED:
-        console.log(action)
-        return {
-          ...state,
-          chartLabels: action.chartLabels,
-          chartData: action.chartData,
-          chartLoading:true
-        };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ACTION_TYPE.ALL_LIST:
+      return {
+        ...state,
+        dataList: action.dataList,
+        loading: action.loading,
+      };
+    case ACTION_TYPE.CHART_DATA_LOADED:
+      console.log(action)
+      return {
+        ...state,
+        chartLabels: action.chartLabels,
+        chartData: action.chartData,
+        chartLoading: true
+      };
+    default:
+      return state;
+  }
 }
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const options = {
-    indexAxis: 'y',
-    elements: {
-        bar: {
-            borderWidth: 2,
-        },
+  indexAxis: 'y',
+  elements: {
+    bar: {
+      borderWidth: 2,
     },
-    responsive: true,
-    plugins: {
-        title: {
-            display: true,
-            text: '서울 2017년 1월 자료',
-        },
+  },
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: '서울 2017년 1월 자료',
     },
+  },
 };
 
 
 const ADHDTableData = () => {
-    const [state, dispatch] = useReducer(dataReducer, {
-      dataList: [],
-      loading: false,
-      chartLoading: false,
-      chartLabels: [],
-      chartData : []
-    }, initData);
+  const [state, dispatch] = useReducer(dataReducer, {
+    dataList: [],
+    loading: false,
+    chartLoading: false,
+    chartLabels: [],
+    chartData: []
+  }, initData);
 
   const data = {
-    labels: !state.chartLoading ? [] : state.chartLabels ,
+    labels: !state.chartLoading ? [] : state.chartLabels,
     datasets: [
       {
         label: 'Dataset 2',
@@ -85,71 +85,71 @@ const ADHDTableData = () => {
     ],
   };
 
-    const renderForecastsTable = (dataList) => {
-        return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>날짜</th>
-                        <th style={{ width: "200px"}}>종류</th>
-                        <th>서울</th>
-                        <th>부산</th>
-                        <th>인천</th>
-                        <th>대구</th>
-                        <th>광주</th>
-                        <th>대전</th>
-                        <th>울산</th>
-                        <th>경기</th>
-                        <th>강원</th>
-                        <th>충북</th>
-                        <th>충남</th>
-                        <th>전북</th>
-                        <th>전남</th>
-                        <th>경북</th>
-                        <th>경남</th>
-                        <th>제주</th>
-                        <th>세종</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dataList.map(data =>
-                        <tr key={data.col01}>
-                            <td>{data.col02}</td>
-                            <td>{data.col03}</td>
-                            <td>{data.col04}</td>
-                            <td>{data.col05}</td>
-                            <td>{data.col06}</td>
-                            <td>{data.col07}</td>
-                            <td>{data.col08}</td>
-                            <td>{data.col09}</td>
-                            <td>{data.col10}</td>
-                            <td>{data.col11}</td>
-                            <td>{data.col12}</td>
-                            <td>{data.col13}</td>
-                            <td>{data.col14}</td>
-                            <td>{data.col15}</td>
-                            <td>{data.col16}</td>
-                            <td>{data.col17}</td>
-                            <td>{data.col18}</td>
-                            <td>{data.col19}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    };
+  const renderForecastsTable = (dataList) => {
+    return (
+      <table className='table table-striped' aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>날짜</th>
+            <th style={{ width: "200px" }}>종류</th>
+            <th>서울</th>
+            <th>부산</th>
+            <th>인천</th>
+            <th>대구</th>
+            <th>광주</th>
+            <th>대전</th>
+            <th>울산</th>
+            <th>경기</th>
+            <th>강원</th>
+            <th>충북</th>
+            <th>충남</th>
+            <th>전북</th>
+            <th>전남</th>
+            <th>경북</th>
+            <th>경남</th>
+            <th>제주</th>
+            <th>세종</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataList.map(data =>
+            <tr key={data.col01}>
+              <td>{data.col02}</td>
+              <td>{data.col03}</td>
+              <td>{data.col04}</td>
+              <td>{data.col05}</td>
+              <td>{data.col06}</td>
+              <td>{data.col07}</td>
+              <td>{data.col08}</td>
+              <td>{data.col09}</td>
+              <td>{data.col10}</td>
+              <td>{data.col11}</td>
+              <td>{data.col12}</td>
+              <td>{data.col13}</td>
+              <td>{data.col14}</td>
+              <td>{data.col15}</td>
+              <td>{data.col16}</td>
+              <td>{data.col17}</td>
+              <td>{data.col18}</td>
+              <td>{data.col19}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  };
 
-    async function initData() {
-      await loadChartData();
+  async function initData() {
+    await loadChartData();
 
-      //const response = await fetch('data/AllList');
-      //  dispatch({
-      //      type: ACTION_TYPE.ALL_LIST,
-      //      dataList: await response.json(),
-      //      loading: true
-      //  });
+    //const response = await fetch('data/AllList');
+    //  dispatch({
+    //      type: ACTION_TYPE.ALL_LIST,
+    //      dataList: await response.json(),
+    //      loading: true
+    //  });
 
-    }
+  }
 
   async function loadChartData() {
     fetch('data/loadChartDataXXX')
@@ -162,24 +162,24 @@ const ADHDTableData = () => {
           chart_loading: true
         });
       })
-    }
+  }
 
 
-    let contents = !state.loading
-        ? <p><em>Loading...</em></p>
-        : renderForecastsTable(state.dataList);
+  let contents = !state.loading
+    ? <p><em>Loading...</em></p>
+    : renderForecastsTable(state.dataList);
 
-    return (
-        <div>
-            <div>
-                <Bar options={options} data={data} />
-            </div>
-            <div>
-                <h1 id="tabelLabel" >Data</h1>
-                {contents}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Bar options={options} data={data} />
+      </div>
+      <div>
+        <h1 id="tabelLabel" >Data</h1>
+        {contents}
+      </div>
+    </div>
+  );
 };
 
 export default ADHDTableData;
---
+++ Models/HospitalDataRepo.cs
@@ -0,0 +1,114 @@
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.models
+{
+    public class DataRepository
+    {
+        private OracleConnection conn;
+
+        public DataRepository()
+        {
+            conn = new OracleConnection("User Id = admin;Password = 1q2w3e4r5tAA;Data Source = orcl_medium");
+            conn.Open();
+        }
+
+        ~DataRepository()
+        {
+            conn.Close();
+        }
+
+        public List<Data> AllList()
+        {
+            //[0] TodoItem을 저장할 목록 객체를 생성한다 
+            List<Data> list = new List<Data>();
+
+            //[1] Command 객체 생성
+            OracleCommand cmd = new OracleCommand();
+
+            //[2] Connection 객체 연결
+            cmd.Connection = conn;
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            //[3] SQL 생성 및 실행 
+            cmd.CommandText = $"select * from HOSPITAL";
+            OracleDataReader dataReader = cmd.ExecuteReader();
+            //[4] 자료를 읽어 객체와 해서 목록 객체에 추가한다 
+            while(dataReader.Read())
+            {
+                //[5] TodoItem 객체를 생성한다
+                Data todoItem = new Data();
+
+                //[6] TodoItem 객체의 속성에 값을 설정한다 
+                todoItem.col01 = dataReader.GetString(0);
+                todoItem.col02 = dataReader.GetString(1);
+                todoItem.col03 = dataReader.GetString(2);
+                todoItem.col04 = dataReader.GetString(3);
+                todoItem.col05 = dataReader.GetString(4);
+                todoItem.col06 = dataReader.GetString(5);
+                todoItem.col07 = dataReader.GetString(6);
+                todoItem.col08 = dataReader.GetString(7);
+                todoItem.col09 = dataReader.GetString(8);
+                todoItem.col10 = dataReader.GetString(9);
+                todoItem.col11 = dataReader.GetString(10);
+                todoItem.col12 = dataReader.GetString(11);
+                todoItem.col13 = dataReader.GetString(12);
+                todoItem.col14 = dataReader.GetString(13);
+                todoItem.col15 = dataReader.GetString(14);
+
+                //[7] 리턴할 목록 객체에 TodoItem 객체를 추가한다
+                list.Add(todoItem);
+            }
+
+            //[8] DB 작업한 것을 정리한다 
+            dataReader.Close();
+            cmd.Dispose();
+
+            //[9] 목록 객체를 리턴한다
+            return list;
+        }
+
+        public Dictionary<string, List<Object>> ChartDataXXX()
+        {
+            Dictionary<string, List<Object>> result = new Dictionary<string, List<Object>>();
+
+            //[1] Command 객체 생성
+            OracleCommand cmd = new OracleCommand();
+
+            //[2] Connection 객체 연결
+            cmd.Connection = conn;
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            //[3] SQL 생성 및 실행 
+            cmd.CommandText = $"select * from HOSPITAL";
+            OracleDataReader dataReader = cmd.ExecuteReader();
+
+            List<Object> chartLabels = new List<Object>();
+            List<Object> chartData = new List<Object>();
+
+            //[4] 자료를 읽어 객체와 해서 목록 객체에 추가한다 
+            while (dataReader.Read())
+            {
+                //[5] TodoItem 객체를 생성한다
+                Data todoItem = new Data();
+
+                //[6] TodoItem 객체의 속성에 값을 설정한다 
+                chartLabels.Add(dataReader.GetString(0));
+                chartData.Add(dataReader.GetString(1));
+
+            }
+
+            //[8] DB 작업한 것을 정리한다
+            dataReader.Close();
+            cmd.Dispose();
+
+            result.Add("chartLabels", chartLabels);
+            result.Add("chartData", chartData);
+
+            return result;
+        }
+    }
+}
