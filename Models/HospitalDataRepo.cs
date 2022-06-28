27ee713e74773978f1201c521a8a4cda338cb19d
yuhs0383 <yuhs0383@naver.com>
2022-06-28 19:50:35 +0900

    conflick 수정

--- Models/HospitalDataRepo.cs
+++ Models/HospitalDataRepo.cs
@@ -12,7 +12,7 @@ namespace Hospital.models
 
         public HospitalDataRepo()
         {
-            conn = new OracleConnection("User Id = admin;Password = 1q2w3e4r5tAA;Data Source = orcl_medium");
+            conn = new OracleConnection("User Id = admin;Password = 1q2w3e4r5tAAA;Data Source = orcl_medium");
             conn.Open();
         }
 
@@ -23,7 +23,7 @@ namespace Hospital.models
 
         public List<HospitalData> AllList()
         {
-            //[0] TodoItem을 저장할 목록 객체를 생성한다 
+            //[0] hospital을 저장할 목록 객체를 생성한다 
             List<HospitalData> list = new List<HospitalData>();
 
             //[1] Command 객체 생성
@@ -39,28 +39,28 @@ namespace Hospital.models
             //[4] 자료를 읽어 객체와 해서 목록 객체에 추가한다 
             while(dataReader.Read())
             {
-                //[5] TodoItem 객체를 생성한다
-                HospitalData todoItem = new HospitalData();
+                //[5] hospital 객체를 생성한다
+                HospitalData hospital = new HospitalData();
 
-                //[6] TodoItem 객체의 속성에 값을 설정한다 
-                todoItem.col01 = dataReader.GetString(0);
-                todoItem.col02 = dataReader.GetString(1);
-                todoItem.col03 = dataReader.GetString(2);
-                todoItem.col04 = dataReader.GetString(3);
-                todoItem.col05 = dataReader.GetString(4);
-                todoItem.col06 = dataReader.GetString(5);
-                todoItem.col07 = dataReader.GetString(6);
-                todoItem.col08 = dataReader.GetString(7);
-                todoItem.col09 = dataReader.GetString(8);
-                todoItem.col10 = dataReader.GetString(9);
-                todoItem.col11 = dataReader.GetString(10);
-                todoItem.col12 = dataReader.GetString(11);
-                todoItem.col13 = dataReader.GetString(12);
-                todoItem.col14 = dataReader.GetString(13);
-                todoItem.col15 = dataReader.GetString(14);
+                //[6] hospital 객체의 속성에 값을 설정한다 
+                hospital.col01 = dataReader.GetString(0);
+                hospital.col02 = dataReader.GetString(1);
+                hospital.col03 = dataReader.GetInt32(2);
+                hospital.col04 = dataReader.GetString(3);
+                hospital.col05 = dataReader.GetString(4);
+                hospital.col06 = dataReader.GetString(5);
+                hospital.col07 = dataReader.GetString(6);
+                hospital.col08 = dataReader.GetString(7);
+                hospital.col09 = dataReader.GetString(8);
+                hospital.col10 = dataReader.GetString(9);
+                hospital.col11 = dataReader.GetString(10);
+                hospital.col12 = dataReader.GetString(11);
+                hospital.col13 = dataReader.GetString(12);
+                hospital.col14 = dataReader.GetDouble(13);
+                hospital.col15 = dataReader.GetDouble(14);
 
-                //[7] 리턴할 목록 객체에 TodoItem 객체를 추가한다
-                list.Add(todoItem);
+                //[7] 리턴할 목록 객체에 hospital 객체를 추가한다
+                list.Add(hospital);
             }
 
             //[8] DB 작업한 것을 정리한다 
