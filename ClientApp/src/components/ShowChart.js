196c7965043c3da1aa737c8273521b970a20e226
민제 정 <tobesincere@naver.com>
2022-07-19 19:21:12 +0900

    라인을 곡선으로 수정, 색상변경

--- ClientApp/src/components/ShowChart.js
+++ ClientApp/src/components/ShowChart.js
@@ -25,11 +25,12 @@ const ShowChart = ({ state }) => {
   const data = {
     labels: state.chartLabels,
     datasets: [
-      {
+        {
+        lineTension : 0.5,
         label: "First Dataset",
         data: state.chartData,
-        borderColor: "black",
-        backgroundColor: "yellow",
+        borderColor: "rgba(75,192,192,1)",
+        backgroundColor: "rgba(75,192,192,0.2)",
       },
     ],
   };
